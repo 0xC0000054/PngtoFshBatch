5705ad6bd5c0d80984129cca9e2bd778eaaecc0e
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2011-01-02 20:11:19 +0000

    Update Copyright year to 2011

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("PngtoFshBatch")]
-[assembly: AssemblyCopyright("Copyright © 2010")]
+[assembly: AssemblyCopyright("Copyright © 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
