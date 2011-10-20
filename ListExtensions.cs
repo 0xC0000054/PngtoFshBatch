27dab0a16ae01b7b15ece71e487de593a76b0c6f
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2011-10-20 20:21:28 +0000

    Updated to use different overloads  for the List Types

--- ListExtensions.cs
+++ ListExtensions.cs
@@ -7,13 +7,21 @@ namespace PngtoFshBatchtxt
 {
     static class ListExtensions
     {
-        public static void SetCapacity<T>(this List<T> list, int newCapacity)
+        public static void SetCapacity(this List<string> list, int newCapacity)
         {
             if (list.Capacity < newCapacity)
             {
                 list.Capacity = newCapacity;
             }
         }
+        public static void SetCapacity(this List<BatchFshContainer> list, int newCapacity)
+        {
+            if (list.Capacity < newCapacity)
+            {
+                list.Capacity = newCapacity;
+            }
+        }
+
 
     }
 }
