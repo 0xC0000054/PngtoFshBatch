2375449ff110a7c8624ec508b7492db7cbd5cb08
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2011-10-20 06:22:19 +0000

    Initial Commit

---
+++ ListExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PngtoFshBatchtxt
+{
+    static class ListExtensions
+    {
+        public static void SetCapacity<T>(this List<T> list, int newCapacity)
+        {
+            if (list.Capacity < newCapacity)
+            {
+                list.Capacity = newCapacity;
+            }
+        }
+
+    }
+}
