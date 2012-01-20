1385ae792c0646711a309ad4d058183ba317551b
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2012-01-20 07:05:13 +0000

    Updated to set the raw data in the FshImageWrapper.

---
+++ FSHImageWrapperExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FSHLib;
+using System.Reflection;
+using FshDatIO;
+
+namespace PngtoFshBatchtxt
+{
+    static class FSHImageWrapperExtensions
+    {
+        private static FieldInfo rawDataField;
+        public static void SetRawData(this FSHImageWrapper fsh, byte[] data)
+        {
+            if (rawDataField == null)
+	        {
+		        Type fshImageType = typeof(FSHImageWrapper);
+                rawDataField = fshImageType.GetField("rawData", BindingFlags.Instance | BindingFlags.NonPublic);
+	        }
+            rawDataField.SetValue(fsh, data);
+        }
+    }
+}
