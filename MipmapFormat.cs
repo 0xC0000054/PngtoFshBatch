29cd2e2f6b9a9312ca4de6735b3d72d79198f48e
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2012-05-11 09:03:53 +0000

    Initial Commit.

---
+++ MipmapFormat.cs
@@ -0,0 +1,21 @@
+﻿namespace PngtoFshBatchtxt
+{
+    /// <summary>
+    /// The format of the mipmaps
+    /// </summary>
+    internal enum MipmapFormat
+    {
+        /// <summary>
+        /// The mipmaps are in seperate files. 
+        /// </summary>
+        Normal,
+        /// <summary>
+        /// The mipmaps are after the main image (used by most automata).
+        /// </summary>
+        Embedded,
+        /// <summary>
+        /// No mipmaps are generated
+        /// </summary>
+        None
+    }
+}
