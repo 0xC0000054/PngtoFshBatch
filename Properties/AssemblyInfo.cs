8079bad59868381cd1fce4031a344d225b4ae5e6
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2013-04-06 00:57:40 +0000

    File version updated to 1.5.1.2.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("null45")]
 [assembly: AssemblyProduct("PngtoFshBatch")]
-[assembly: AssemblyCopyright("Copyright © 2009-2012 Nicholas Hayes")]
+[assembly: AssemblyCopyright("Copyright © 2009-2013 Nicholas Hayes")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.5.1.1")]
-[assembly: AssemblyFileVersion("1.5.1.1")]
+[assembly: AssemblyVersion("1.5.1.2")]
+[assembly: AssemblyFileVersion("1.5.1.2")]
