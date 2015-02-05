ea0e489bcead0743dba49a820daff6a12dec3066
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2015-02-05 03:57:04 +0000

    Initial commit.

---
+++ StringExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PngtoFshBatchtxt
+{
+    internal static class StringExtensions
+    {
+        internal static bool Contains(this string s, string value, StringComparison comparisonType)
+        {
+            return s.IndexOf(value, comparisonType) >= 0;
+        }
+    }
+}
