4330446c88eacc90a53d4bb97da61cf523fb61dc
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2015-03-02 05:42:45 +0000

    Initial commit.

---
+++ ListViewItemComparer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace PngtoFshBatchtxt
+{
+    // Implements the manual sorting of items by columns.
+    internal sealed class ListViewItemComparer : IComparer<ListViewItem>
+    {
+        private int col;
+        private SortOrder order;
+
+        public ListViewItemComparer(int column, SortOrder order)
+        {
+            this.col = column;
+            this.order = order;
+        }
+
+        public int Compare(ListViewItem x, ListViewItem y)
+        {
+            if (x == null)
+            {
+                throw new ArgumentNullException("x");
+            }
+
+            if (y == null)
+            {
+                throw new ArgumentNullException("y");
+            }
+
+            int returnVal = StringLogicalComparer.Compare(x.SubItems[col].Text, y.SubItems[col].Text);
+
+            // Determine whether the sort order is descending.
+            if (order == SortOrder.Descending)
+            {     
+                // Invert the value returned by String.Compare.
+                returnVal *= -1;
+            }
+            
+            return returnVal;
+        }
+    }
+
+}
