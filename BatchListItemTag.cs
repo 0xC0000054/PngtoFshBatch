0f81b97aaf8d29c0e65ce02445cb004555a19d31
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2015-03-02 04:57:50 +0000

    Initial commit.

---
+++ BatchListItemTag.cs
@@ -0,0 +1,38 @@
+﻿using FshDatIO;
+using System.Drawing;
+
+namespace PngtoFshBatchtxt
+{
+    internal sealed class BatchListItemTag
+    {
+        private readonly Size mainImageSize;
+        private FshImageFormat format;
+
+
+        public Size MainImageSize
+        {
+            get
+            {
+                return this.mainImageSize;
+            }
+        }
+
+        public FshImageFormat Format
+        {
+            get
+            {
+                return this.format;
+            }
+            set
+            {
+                this.format = value;
+            }
+        }
+
+        public BatchListItemTag(Size mainImageSize, FshImageFormat format)
+        {
+            this.mainImageSize = mainImageSize;
+            this.format = format;
+        }
+    }
+}
